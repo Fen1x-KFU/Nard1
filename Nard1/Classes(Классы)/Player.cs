6f9ec8c5ba435338f3a8f2e1cc7112f93ab79c60
Андрей Мингалёв <ammingalev@gmail.com>
2025-05-20 00:58:53 +0300

    Добавьте файлы проекта.

---
+++ Nard1/Classes(Классы)/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nard
+{
+    /// <summary>
+    /// Класс игрока
+    /// </summary>
+    internal class Player
+    {
+        /// <summary>
+        /// Доска игрока (Словарь -- номер ячейки : (кем занята и кол-во фишек))
+        /// </summary>
+        public Dictionary<int, byte[][]> Desk {  get; set; }
+        /// <summary>
+        /// Кубик 1
+        /// </summary>
+        public byte Cube1 { get; set; }
+        /// <summary>
+        /// Кубик 2
+        /// </summary>
+        public byte Cube2 { get; set; }
+        
+    }
+}
