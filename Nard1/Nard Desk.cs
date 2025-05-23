b5232f73d3d3d2b652d979370d816f36e5ed60d7
AngelaPushkina <puskinaanzela06@gmail.com>
2025-05-23 22:48:50 +0300

    Добавил панель зелёного цвета

--- Nard1/Nard Desk.cs
+++ Nard1/Nard Desk.cs
@@ -1,9 +1,13 @@
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace Nard1
 {
     public partial class Form1 : Form
     {
+        PictureBox pictureBox = new PictureBox();
+        Panel panel = new Panel();
+
         public Form1()
         {
             InitializeComponent();
@@ -11,6 +15,7 @@ namespace Nard1
             this.WindowState = FormWindowState.Maximized;
             this.MaximizeBox = false; // Отключает кнопку развёртывания
             SetupPictureBox();
+            CreatePanel();
         }
 
         public void ImagBackGround()
@@ -24,7 +29,7 @@ namespace Nard1
         private void SetupPictureBox()
         {
             // Создаём PictureBox
-            PictureBox pictureBox = new PictureBox();
+            //PictureBox pictureBox = new PictureBox();
 
             // Устанавливаем размер (например, 200x200)
             pictureBox.Size = new Size(200, 200);
@@ -58,6 +63,41 @@ namespace Nard1
 
             // Добавляем PictureBox на форму
             this.Controls.Add(pictureBox);
+        }
+
+        private void CreatePanel()
+        {
+            // Рассчитываем дополнительную ширину (4 см в пикселях)
+            float cmToPixels = 37.8f; // Примерное значение
+            int extraWidth = (int)(4 * cmToPixels);
+
+            // Устанавливаем размер панели (шире pictureBox на 4 см с каждой стороны)
+            panel.Size = new Size(
+                pictureBox.Width + (2 * extraWidth),
+                pictureBox.Height
+            );
+
+            // Позиционируем панель по центру формы
+            panel.Location = new Point(
+                (this.ClientSize.Width - panel.Width) / 2,
+                (this.ClientSize.Height - panel.Height) / 2
+            );
+
+            // Настройка внешнего вида
+            panel.BackColor = Color.Green; // Зелёный цвет
+            panel.BorderStyle = BorderStyle.FixedSingle; // Рамка для наглядности
+
+            // Обработчик изменения размера формы
+            this.Resize += (sender, e) =>
+            {
+                panel.Location = new Point(
+                    (this.ClientSize.Width - panel.Width) / 2,
+                    (this.ClientSize.Height - panel.Height) / 2
+                );
+            };
+
+            // Добавляем панель на форму
+            this.Controls.Add(panel);
         }
     }
 }
